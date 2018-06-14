76a594bb0f363108e1b73c4654bb90d010082cfa
Joey Cho <cjy12182000@naver.com>
2018-06-14 17:00:30 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -17,7 +17,9 @@ class Song
   end
 
   def genre=(genre)
-    if genre == nil || !genre.songs.include?(self)
+    if genre == nil       
+      genre.songs << self
+    elsif genre.songs.include?(self)
       genre.songs << self
     else
     end
