e6a2c0e23641ae484e17bd7d53c72c72b39eb24e
Joey Cho <cjy12182000@naver.com>
2018-06-14 20:16:37 +0000

    automatically backed up by learn

--- lib/musiclibrarycontroller.rb
+++ lib/musiclibrarycontroller.rb
@@ -106,8 +106,9 @@ class MusicLibraryController
       narr = Song.all.sort do
             |sone, stwo| sone.name <=> stwo.name
           end
-        #  binding.pry
+        
       if @song_n.to_i >0 && @song_n.to_i < narr.length
+          binding.pry
       puts("Playing #{narr['#{@song_n}'.to_i-1].name} by #{narr['#{@song_n}'.to_i-1].artist.name}")
       end
     end
