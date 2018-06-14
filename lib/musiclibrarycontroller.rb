9b907f555508b1bb7ab6425c69397939bdc260da
Joey Cho <cjy12182000@naver.com>
2018-06-14 19:44:04 +0000

    automatically backed up by learn

--- lib/musiclibrarycontroller.rb
+++ lib/musiclibrarycontroller.rb
@@ -64,7 +64,7 @@ class MusicLibraryController
   def list_songs_by_artist
 
     puts("Please enter the name of an artist:")
-    
+    @artist_n = gets
     i = 0
     narr = Song.all.sort do
           |sone, stwo| sone.name <=> stwo.name
