0fb8676f041216441dbe4cab8b947840ae67dde5
Joey Cho <cjy12182000@naver.com>
2018-06-14 16:49:45 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -38,13 +38,9 @@ class Artist
     if song.artist == nil
     song.artist = self
     end
-    
+
     self.songs << song
     song.class.all << song
-    else
-
-    end
-
     end
 
     def genres
