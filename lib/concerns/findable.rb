5b8649b5d6f025d157b0ade5e2e58454b4b5bd58
Joey Cho <cjy12182000@naver.com>
2018-06-14 17:37:04 +0000

    automatically backed up by learn

--- lib/concerns/findable.rb
+++ lib/concerns/findable.rb
@@ -1,5 +1,18 @@
 module Concerns
   module Findable
-    
+
+    def self.find_by_name(name)
+      self.all.detect do
+        |song| song.name == name
+      end
+    end
+
+    def self.find_or_create_by_name(name)
+      if self.find_by_name(name) == nil
+        self.create(name)
+      else
+        self.find_by_name(name)
+      end
+    end
   end
 end
