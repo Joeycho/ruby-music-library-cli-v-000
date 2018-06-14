cf878c501670d4329e867a915396d34916fad128
Joey Cho <cjy12182000@naver.com>
2018-06-14 16:23:18 +0000

    automatically backed up by learn

---
+++ lib/artist.rb
@@ -0,0 +1,57 @@
+require 'pry'
+
+class Artist
+
+  attr_accessor :name
+
+  @@all = []
+
+
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.destroy_all
+    self.all.clear
+  end
+
+  def save
+    @@all << self
+  end
+
+  def songs
+    @songs
+  end
+
+  def self.create(name)
+    song =self.new(name)
+    song.save
+    song
+  end
+
+  def add_song(song)
+    if song.artist == nil
+    song.artist = self
+    self.songs << song
+    song.class.all << song
+    else
+
+    end
+
+    end
+
+    def genres
+      genres =self.songs.map do
+        |song| song.genre
+      end
+      binding.pry
+      genres.uniq
+    end
+
+
+end
---
+++ lib/song.rb
@@ -0,0 +1,59 @@
+require 'pry'
+
+class Song
+
+  attr_accessor :name, :artist, :genre
+
+  @@all = []
+
+
+  def initialize(name, artist=nil, genre=nil)
+    @name = name
+
+    if genre != nil
+    self.genre = genre
+    end
+
+    if artist != nil
+    self.artist = artist
+    artist.add_song(self)
+    end
+  end
+
+  def genre=(genre)
+    if genre.songs.include?(self)
+    else
+    genre.songs << self
+    end
+    @genre = genre
+  end
+
+  def genre
+    @genre
+  end
+
+  def artist=(artist)
+    @artist = artist
+    @artist.add_song(self)
+  end
+
+
+  def self.all
+    @@all
+  end
+
+  def self.destroy_all
+    self.all.clear
+  end
+
+  def save
+    @@all << self
+  end
+
+  def self.create(name)
+    song =self.new(name)
+    song.save
+    song
+  #  binding.pry
+  end
+end
